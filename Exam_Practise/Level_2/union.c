902bf2d4b4ded729d6224579e7973ca424cc1eb9
ParidGjerasi <ridigjerasi@gmail.com>
2026-01-13 18:45:40 +0100

    Updated exercises

--- Exam_Practise/Level_2/union.c
+++ Exam_Practise/Level_2/union.c
@@ -1,34 +1,29 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
+int main(int ac, char **av){
+
     int i;
-    int printed[256] = {0};
-    
-    if (argc == 3)
-    {
-        i = 0;
-        while (argv[1][i])
-        {
-            if (printed[(unsigned char)argv[1][i]] == 0)
-            {
-                write(1, &argv[1][i], 1);
-                printed[(unsigned char)argv[1][i]] = 1;
+    int seen[256] = {0};
+
+    if(ac ==3){
+
+        i=0;
+        while(av[1][i]){
+            if(seen[(unsigned char)av[1][i]]==0){
+                write(1, &av[1][i], 1);
+                seen[(unsigned char)av[1][i]]=1;
             }
             i++;
         }
-        
-        i = 0;
-        while (argv[2][i])
-        {
-            if (printed[(unsigned char)argv[2][i]] == 0)
-            {
-                write(1, &argv[2][i], 1);
-                printed[(unsigned char)argv[2][i]] = 1;
+
+        i=0;
+          while(av[2][i]){
+            if(seen[(unsigned char)av[2][i]]==0){
+                write(1, &av[2][i], 1);
+                seen[(unsigned char)av[2][i]]=1;
             }
             i++;
-        }
     }
+}
     write(1, "\n", 1);
-    return (0);
 }
